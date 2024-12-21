1b7779f82f9bb3254434826e0ddfb0d0b68c0437
Carlos A <cayala.w@gmail.com>
2024-12-21 00:49:07 -0500

    streamline request model

--- src/com/alura/swapi/main/MovieSearch.java
+++ src/com/alura/swapi/main/MovieSearch.java
@@ -1,6 +1,5 @@
 package com.alura.swapi.main;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,24 +10,19 @@ import com.alura.swapi.models.Movie;
 import com.google.gson.Gson;
 
 public class MovieSearch {
-  Movie movieSearch(int movieNumber) {
+  public Movie movieSearch(int movieNumber) {
     URI address = URI.create("https://swapi.py4e.com/api/films/" + movieNumber + '/');
-    // System.out.println("address:" + address);
 
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
         .uri(address)
         .build();
 
-    HttpResponse<String> response = null;
-
     try {
-      response = client.send(request, BodyHandlers.ofString());
-      // System.out.println("response body: " + response.body());
-    } catch (IOException | InterruptedException e) {
-      throw new RuntimeException(e);
+      HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+      return new Gson().fromJson(response.body(), Movie.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Movie not found.");
     }
-
-    return new Gson().fromJson(response.body(), Movie.class);
   }
 }
